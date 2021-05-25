a6bbb1b9533dd8bb3967c2716609257cdc4f1eb1
marquinamq <petmalugasmati21@gmail.com>
2021-05-25 14:42:44 +0800

    test

--- resources/views/payslip/create.blade.php
+++ resources/views/payslip/create.blade.php
@@ -3,39 +3,39 @@
 @section('title', 'Payslip')
 @section('title', $active)
 @section('content')
-  <div style="margin-top: -1%;" class="row">
-    <div class="col-12 mt-5">
-      <div class="card">
-          <div class="card-body row"> 
-              <h3>Process Payroll</h3>
-              <hr>
-              <div class="col-10"></div>
-              {{-- <div class="col-2 text-end"> 
+    <div style="margin-top: -1%;" class="row">
+        <div class="col-12 mt-5">
+            <div class="card">
+                <div class="card-body row">
+                    <h3>List Payroll</h3>
+                    <hr>
+                    <div class="col-10"></div>
+                    {{-- <div class="col-2 text-end"> 
                   <button class="btn btn-primary pull-right">Create</button>
-              </div>  --}}
-              <div class="col-12">
-                    <br>
-              </div>
-              {{-- <div class="col-8"></div>
+              </div> --}}
+                    <div class="col-12">
+                        <br>
+                    </div>
+                    {{-- <div class="col-8"></div>
               <div class="col-4 text-end"> 
                   <input type="text" class="form-control" placeholder="Search">
-              </div>  --}}
-              <div class="col-12">
-                <form class="row g-3">
-                    <div class="col-md-6">
-                      <label for="inputEmail4" class="form-label">Cut-off start</label>
-                      <input type="date" class="form-control" id="inputEmail4">
-                    </div>
-                    <div class="col-md-6">
-                      <label for="inputPassword4" class="form-label">Cut-off end</label>
-                      <input type="date" class="form-control" id="inputPassword4">
-                    </div>
+              </div> --}}
                     <div class="col-12">
-                      <button type="submit" class="btn btn-primary">Save</button>
+                        <form class="row g-3">
+                            <div class="col-md-6">
+                                <label for="inputEmail4" class="form-label">Cut-off start</label>
+                                <input type="date" class="form-control" id="inputEmail4">
+                            </div>
+                            <div class="col-md-6">
+                                <label for="inputPassword4" class="form-label">Cut-off end</label>
+                                <input type="date" class="form-control" id="inputPassword4">
+                            </div>
+                            <div class="col-12">
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </div>
+                        </form>
                     </div>
-                  </form>
-              </div>
-          </div>
-      </div>
-  </div> 
-  @endsection  
+                </div>
+            </div>
+        </div>
+    @endsection
